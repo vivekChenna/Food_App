fe1c9eb077092e04044035a5563488fedbf5d326
vivekChenna <chennavivek96@gmail.com>
2023-02-11 02:06:30 +0530

    completed building the accordion

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,65 @@
+import { useState } from "react";
+
+const Section = ({ title, desc, isVisible1, setIsVisible }) => {
+  return (
+    <div className="section-block">
+      <h2>{title}</h2>
+      {isVisible1 ? (
+        <>
+          <div className="description-box">
+            <p>{desc}</p>
+          </div>
+          <button
+            className="section-block-btn"
+            onClick={() => {
+              setIsVisible(false);
+            }}
+          >
+            Hide
+          </button>
+        </>
+      ) : (
+        <button
+          className="section-block-btn"
+          onClick={() => {
+            setIsVisible(title); // Team
+          }}
+        >
+          Show
+        </button>
+      )}
+    </div>
+  );
+};
+
 const Contact = () => {
+  const [isVisible, setIsVisible] = useState("Team");
   return (
     <div>
-      <h1>this is the contact us page</h1>
+      <Section
+        title={"Team"}
+        desc={
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+        }
+        isVisible1={isVisible === "Team"}
+        setIsVisible={setIsVisible}
+      />
+      <Section
+        title={"Project"}
+        desc={
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+        }
+        isVisible1={isVisible === "Project"}
+        setIsVisible={setIsVisible}
+      />
+      <Section
+        title={"collaboration"}
+        desc={
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+        }
+        isVisible1={isVisible === "collaboration"}
+        setIsVisible={setIsVisible}
+      />
     </div>
   );
 };
